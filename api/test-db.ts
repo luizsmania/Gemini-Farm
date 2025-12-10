ec6f9633bcca6baa6bbb6442d053ea36539b7e40
Luiz Henrique <luizsmania1@gmail.com>
2025-12-10 16:43:29 +0000

    deployed

--- api/test-db.ts
+++ api/test-db.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { sql, initDatabase, usernameExists } from './database';
+import { sql } from '@vercel/postgres';
+import { initDatabase, usernameExists } from './database';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
